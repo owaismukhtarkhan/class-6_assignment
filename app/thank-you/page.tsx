8f13e83b65a2f8b4fb28d2ce43fb1cbb1cb99673
Owais Mukhtar Khan <owaismukhtarkhan@gmail.com>
2024-10-29 06:56:46 +0500

    Update page.tsx

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,7 +1,7 @@
 export default function Thank_You() {
     return (
       <div className="bg-[#ffffff]">
-        <h1 className="text-5xl text-center font-extrabold text-[#233d7b] mt-1 pt-40">
+        <h1 className="text-5xl text-center italic font-extrabold text-[#233d7b] mt-1 pt-40">
           Thank You For Shopping!!!
         </h1>
         <p className="text-lg text-center  text-[#233d7b] pt-12 pb-64">Your order will be delivered in 03 Business days</p>
